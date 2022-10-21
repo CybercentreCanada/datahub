58c07e1dadc560fb4e45db4db153621662cb99fc
cccs-Dustin <96579982+cccs-Dustin@users.noreply.github.com>
2022-10-21 08:51:00 -0400

    Modify SourceConfig and SourceReport

--- metadata-ingestion/src/datahub/ingestion/source/iceberg/iceberg_common.py
+++ metadata-ingestion/src/datahub/ingestion/source/iceberg/iceberg_common.py
@@ -6,6 +6,7 @@ import pydantic
 from azure.storage.filedatalake import FileSystemClient, PathProperties
 from iceberg.core.filesystem.abfss_filesystem import AbfssFileSystem
 from iceberg.core.filesystem.filesystem_tables import FilesystemTables
+import pydantic
 from pydantic import Field, root_validator
 
 from datahub.configuration.common import (
@@ -13,6 +14,12 @@ from datahub.configuration.common import (
     ConfigModel,
     ConfigurationError,
 )
+from datahub.ingestion.source.state.stale_entity_removal_handler import StatefulStaleMetadataRemovalConfig, StaleEntityRemovalSourceReport
+from datahub.ingestion.source.state.stateful_ingestion_base import (
+    StatefulIngestionConfigBase,
+)
+from datahub.configuration.source_common import DatasetSourceConfigBase
+from datahub.ingestion.api.source import SourceReport
 from datahub.ingestion.source.azure.azure_common import AdlsSourceConfig
 from datahub.ingestion.source.state.stale_entity_removal_handler import (
     StaleEntityRemovalSourceReport,
