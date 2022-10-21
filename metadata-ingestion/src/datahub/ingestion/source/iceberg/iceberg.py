5b9efafcc81d5cd37e27ba3f602dfb5a252a3955
cccs-Dustin <96579982+cccs-Dustin@users.noreply.github.com>
2022-10-21 09:22:34 -0400

    Add entities from current run to the state object

--- metadata-ingestion/src/datahub/ingestion/source/iceberg/iceberg.py
+++ metadata-ingestion/src/datahub/ingestion/source/iceberg/iceberg.py
@@ -160,6 +160,7 @@ class IcebergSource(StatefulIngestionSourceBase):
             urn=dataset_urn,
             aspects=[],
         )
+        self.stale_entity_removal_handler.add_entity_to_state(type="table", urn=dataset_urn)
 
         custom_properties: Dict = dict(table.properties())
         custom_properties["location"] = table.location()
